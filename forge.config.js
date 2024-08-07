95378b76335c1f7965005be9dec3244439593fc4
HB9HIL <fabian.berg@hb9hil.org>
2024-08-08 00:14:41 +0200

    set correct repo and branch

--- forge.config.js
+++ forge.config.js
@@ -10,7 +10,7 @@ module.exports = {
 			name: '@electron-forge/publisher-github',
 			config: {
 				repository: {
-					owner: 'HB9HIL',
+					owner: 'wavelog',
 					name: 'WaveLogGate'
 				},
 				prerelease: false
