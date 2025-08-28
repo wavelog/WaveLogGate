bb1c4f1bdfebd633a0eaf7377931e58d6c1fcc26
Joerg <joerg@dj7nt.de>
2025-08-28 17:42:21 +0200

    Back to original forge-config

--- forge.config.js
+++ forge.config.js
@@ -10,7 +10,7 @@ module.exports = {
 			name: '@electron-forge/publisher-github',
 			config: {
 				repository: {
-					owner: 'int2001',
+					owner: 'wavelog',
 					name: 'WaveLogGate'
 				},
 				prerelease: false
